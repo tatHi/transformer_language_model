28d2e18186f7259d7c30c30b785e2bb69c732bf9
tatHi <hiraoka.tatsuya.hq7@is.naist.jp>
2018-09-20 18:10:43 +0900

    add argparser

--- src/config.py
+++ src/config.py
@@ -1,26 +1,32 @@
 import argparse
-'''
-args = argparse.ArgumentParser()
 
-args.add_argument('--dk',type=int,default=64)
-args.add_argument('--dk',type=int,default=64)
-args.add_argument('--dk',type=int,default=64)
-'''
 
-d_k = 64
-d_v = 64
-d_word_vec = 512
-d_inner = 1024
-n_heads = 8
-d_model = d_k*n_heads
-n_layers = 6
-n_warmup_steps = 4000
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-d_k',type=int,default=64)
+parser.add_argument('-d_v',type=int,default=64)
+parser.add_argument('-d_inner',type=int,default=1024)
+parser.add_argument('-d_model',type=int,default=512)
+parser.add_argument('-n_layers',type=int,default=6)
+parser.add_argument('-n_heads',type=int,default=8)
+parser.add_argument('-n_warmup_steps',type=int,default=4000)
+parser.add_argument('-maxEpoch',type=int,default=1000)
+
+args = parser.parse_args()
+
+d_k = args.d_k
+d_v = args.d_v
+d_inner = args.d_inner
+n_heads = args.n_heads
+d_model = args.d_model # recomended (d_k * n_heads)
+d_word_vec = args.d_model
+n_layers = args.n_layers
+n_warmup_steps = args.n_warmup_steps
+maxEpoch = args.maxEpoch
 
 trainPath = '../data/ptb.train.txt'
 validPath = '../data/ptb.valid.txt'
 testPath = '../data/ptb.test.txt'
 dictPath = '../data/ptb.train.dict'
-
-maxEpoch = 1000
 
 pad_id = 0
